46ff89e19b90958bb9b20eb435cd15a0ad937b7c
Chris Davison <c.jr.davison@gmail.com>
2019-04-21 12:01:29 +0100

    Convert dashboard books route to sqlite

--- books.py
+++ books.py
@@ -1,68 +1,62 @@
 """Routes for my reading list"""
 import json
 import os
+import sqlite3
+from pathlib import Path
+
 from flask import request, render_template, Blueprint
+
 
 BP_BOOKS = Blueprint("books", __name__, template_folder="templates")
 BOOKS_FILE = os.path.join(os.environ["DATADIR"], "reading-list.json")
-
-
-class Books:
-    """Books represents a list of books"""
-
-    def __init__(self, filename):
-        """Load in the JSON file"""
-        self.filename = filename
-        self.data = []
-        for b in json.load(open(BOOKS_FILE)):
-            b["Read"] = "" if not b["Read"] else b["Read"]
-            b["Author"] = "" if not b["Author"] else b["Author"]
-            self.data.append(b)
-
-    def write(self):
-        """If data exists, write it to file"""
-        if self.data:
-            json.dump(self.data, open(self.filename, "w"), indent=2)
-        else:
-            print("NO DATA")
-
-    def append(self, item):
-        """Append an item to the internal data"""
-        self.data.append(item)
+DB_PATH = str((Path(os.environ["DATADIR"]) / "data.db").resolve())
 
 
 @BP_BOOKS.route("/books/")
 def book_list():
     """Render a list of books, with live filtering"""
-    books_list = Books(BOOKS_FILE).data
-    categories = sorted(set([c["Genre"] for c in books_list]))
+    db = sqlite3.connect(DB_PATH)
+    cursor = db.cursor()
+    cursor.execute("""select distinct genre from reading order by genre""")
+    genres = cursor.fetchall()
     out = []
-    for category in categories:
-        books_for_cat = [b for b in books_list if b["Genre"] == category]
-        sorted_by_title = sorted(books_for_cat, key=lambda x: x["Title"])
-        out.extend(sorted_by_title)
+    for genre in genres:
+        cursor.execute(f"""
+            select * from reading
+            where genre='{genre[0]}'
+            order by title""")
+        data = cursor.fetchall()
+        if data:
+            out.extend([{
+                'Title': title,
+                'Author': author,
+                'Genre': genre,
+                'Status': status,
+                'Read': read}
+                for (_, title, author, genre, status, read) in data
+            ])
+    db.close()
     return render_template("books.html", books=out)
 
 
 @BP_BOOKS.route("/books/new", methods=["POST"])
 def new():
     """Add a new book, from the /books/add/ endpoint, to the file"""
-    books = Books(BOOKS_FILE)
-    books.append(
-        {
-            "Title": request.form["title"],
-            "Author": request.form["author"],
-            "Genre": request.form["genre"],
-            "Status": request.form["status"],
-            "Read": request.form["read"],
-        }
-    )
-    books.write()
-    return render_template(
-        "books.html",
-        books=books.data[-10:],
-        extra_pre=f"Added: {request.form['title']} by {request.form['author']}",
-    )
+    db = sqlite3.connect(DB_PATH)
+    cursor = db.cursor()
+    cursor.execute(f"""
+        INSERT INTO reading(title, author, genre, status, read)
+        VALUES (
+            '{request.form["title"]}',
+            '{request.form["author"]}',
+            '{request.form["genre"]}',
+            '{request.form["status"]}',
+            '{request.form["read"]}'
+        )""")
+    db.commit()
+    db.close()
+
+    return book_list()
 
 
 @BP_BOOKS.route("/books/add")
